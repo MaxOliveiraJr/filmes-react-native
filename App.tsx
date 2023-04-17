5e90ee155a982cf9924d396b04fcf426275cbc39
Max Oliveira <max-oliveira@outlook.com.br>
2023-04-17 20:54:47 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View style={{width: '100%'}}>
+      <Text>Oi</Text>
+    </View>
+  );
+}
+
+export default App;
