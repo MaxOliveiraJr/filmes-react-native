d2f402c9816c38f37aa4ff11b66c91aac80755d9
Max Oliveira <max-oliveira@outlook.com.br>
2023-04-17 21:48:11 -0300

    feat: create structure app

--- App.tsx
+++ App.tsx
@@ -5,14 +5,36 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Text, View} from 'react-native';
+import {Text, View, Button, FlatList, Image} from 'react-native';
 
 function App(): JSX.Element {
+  const [movies, setMovies] = useState();
+
+  const handleLoadButton = async () => {
+    const req = await fetch('https://api.b7web.com.br/cinema/');
+    const json = await req.json();
+
+    if (json) {
+      setMovies(json);
+    }
+  };
   return (
     <View style={{width: '100%'}}>
-      <Text>Oi</Text>
+      <Button title="Carregar Filmes" onPress={handleLoadButton}></Button>
+      <FlatList
+        data={movies}
+        renderItem={({item}) => (
+          <View style={{marginVertical: 20}}>
+            <Image
+              source={{uri: item.avatar}}
+              style={{width: '100%', height: 400}}
+            />
+            <Text>{item.titulo}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.titulo}></FlatList>
     </View>
   );
 }
